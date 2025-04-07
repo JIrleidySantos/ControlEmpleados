85857ba395a829880e242b1d48705eef6eddb173
JIrleidySantos <jirleidysantosm@gmail.com>
2025-04-07 08:43:24 -0500

    Clase Administrador, Inspector y Obrero

--- src/main/java/ec/edu/espoch/controldeempleados/RescursosHumanos/Administrador.java
+++ src/main/java/ec/edu/espoch/controldeempleados/RescursosHumanos/Administrador.java
@@ -8,6 +8,28 @@ package ec.edu.espoch.controldeempleados.RescursosHumanos;
  *
  * @author mundo
  */
-public class Administrador {
-    
+public class Administrador extends Empleado{
+    private String numeroSocial;
+    private int horasExtras;
+    private static final double BONO = 0.40;
+    private static final double PAGO_HORA_EXTRA = 30;
+
+    public Administrador(int codigo, String nombres, String apellidos, String direccion, double sueldoBasico,
+                         String numeroSocial, int horasExtras) {
+       super(codigo, nombres, apellidos, direccion, sueldoBasico);
+        this.numeroSocial = numeroSocial;
+        this.horasExtras = horasExtras;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return sueldoBasico + (BONO * sueldoBasico) + (horasExtras * PAGO_HORA_EXTRA);
+    }
+
+    @Override
+    public void imprimirDatos() {
+        System.out.println("Administrador: " + nombres + " " + apellidos + ", Código: " + codigo +
+                ", N° Social: " + numeroSocial + ", Salario: $" + calcularSalario());
+    }
+
 }
