277ed4b625fb4881b3095ce05b0023ef3a46ced7
OswaldoBifarini <jgbifarini@gmail.com>
2025-04-07 08:17:39 -0500

    implementacion clases empleado y trabajador

--- src/main/java/ec/edu/espoch/controldeempleados/RescursosHumanos/Empleado.java
+++ src/main/java/ec/edu/espoch/controldeempleados/RescursosHumanos/Empleado.java
@@ -8,6 +8,20 @@ package ec.edu.espoch.controldeempleados.RescursosHumanos;
  *
  * @author mundo
  */
-public class Empleado {
-    
+abstract class Empleado {
+
+    protected int codigo;
+    protected String nombres;
+    protected String apellidos;
+    protected String direccion;
+    protected double sueldoBasico;
+
+    public Empleado(int codigo, String nombres, String apellidos, String direccion, double sueldoBasico) {
+        this.codigo = codigo;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.sueldoBasico = sueldoBasico;
+    }
+
 }
--- src/main/java/ec/edu/espoch/controldeempleados/RescursosHumanos/Trabajador.java
+++ src/main/java/ec/edu/espoch/controldeempleados/RescursosHumanos/Trabajador.java
@@ -8,6 +8,20 @@ package ec.edu.espoch.controldeempleados.RescursosHumanos;
  *
  * @author mundo
  */
-public class Trabajador {
-    
+public class Trabajador extends Empleado {
+    private String area;
+    private int horasExtras;
+    private static final double BONO = 0.2;
+    private static final double PAGO_HORA_EXTRA = 10;
+
+    public Trabajador(int codigo, String nombres, String apellidos, String direccion, double sueldoBasico, String area, int horasExtras) {
+        super(codigo, nombres, apellidos, direccion, sueldoBasico);
+        this.area = area;
+        this.horasExtras = horasExtras;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return sueldoBasico + (sueldoBasico * BONO) + (horasExtras * PAGO_HORA_EXTRA);
+    }
 }
